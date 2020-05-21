cb29bf5cff2d7c3b5f4334937a05fba70ec441a0
Harshit Maurya <harshitmaurya903@gmail.com>
2020-05-21 17:34:10 +0530

    Intro to redux

--- src/Component/MainComponent.js
+++ src/Component/MainComponent.js
@@ -3,44 +3,43 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { COMMENTS } from '../shared/comments';
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import AboutUs from "./AboutComponent";
+import { connect } from 'react-redux';                /*this is to coonect my maincomponent to redux store*/
 
-class Main extends Component {
+const mapStateToProps = state => {
+    return {
+      dishes: state.dishes,                       /* Now, this dishes will become available from my Redux Store's state here. */
+      comments: state.comments,
+       promotions: state.promotions,                      /*This mapStateToProps function will then return. What this does is that this will map the Redux Store's state into props that will become available to my component.*/
+      leaders: state.leaders
+    }
+  }
+
+class Main extends Component {      
 
     constructor(props) {
         super(props);
-        this.state = {
-            dishes: DISHES,
-            comments: COMMENTS,
-            promotions: PROMOTIONS,
-            leaders: LEADERS
-
-        };
     }
-
+    
     render() {
 
         const HomePage = () => {
             return (
                 <Home
-                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                    dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
         }
       
     const DishWithId = ({match}) => {
         return(
-            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+              comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
         );
       };
       
@@ -50,10 +49,10 @@ class Main extends Component {
                 }
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
-                    <Route path="/aboutus" component={() => <AboutUs leaders={this.state.leaders} />}/>
+                    <Route path="/aboutus" component={() => <AboutUs leaders={this.props.leaders} />}/>
 
               <Redirect to="/home" />
                 </Switch>
@@ -64,4 +63,4 @@ class Main extends Component {
     }
 }
 
-export default Main; 
+export default withRouter(connect(mapStateToProps)(Main));
---
+++ src/redux/reducer.js
@@ -0,0 +1,17 @@
+// This Reducer.js function is the pure function
+
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+export const initialState = {
+    dishes: DISHES,
+    comments: COMMENTS,
+    promotions: PROMOTIONS,
+    leaders: LEADERS
+};
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+};
